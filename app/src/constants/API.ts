deeb98d2c0fbedfa1255e94643becc88dc65dc0c
Artem Kozlov <74830063+Artem1441@users.noreply.github.com>
2023-12-18 11:15:24 +0500

    Update API.ts

--- app/src/constants/API.ts
+++ app/src/constants/API.ts
@@ -1,13 +1,7 @@
 const API = {
     Url: "http://localhost:8080",
     Api: "http://localhost:8080/api",
-    Ws: "ws://localhost:8080",
-    // Url: "https://8db9-46-146-41-95.ngrok-free.app",
-    // Api: "https://8db9-46-146-41-95.ngrok-free.app/api",
-    // Ws: "ws://8db9-46-146-41-95.ngrok-free.app",
-    // Url: "http://37.140.197.124:83",
-    // Api: "http://37.140.197.124:83/api",
-    // Ws: "ws://37.140.197.124:83",
+    Ws: "ws://localhost:8080"
 }
 
 export default API
